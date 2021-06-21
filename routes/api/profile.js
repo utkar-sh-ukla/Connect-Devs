0196722ca326ad144ef0425a0cb986889d93d645
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-06-21 15:11:10 +0530

    Registered Oauth Request on Github & accese its repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const mongoose = require('mongoose');
 const auth = require('../../middleware/auth');
@@ -298,4 +300,34 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   }
 });
 
+//@route    GET api/profile/github/:username
+//@desc     GET user repos from github
+//@access   Public
+
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Giyhub profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
