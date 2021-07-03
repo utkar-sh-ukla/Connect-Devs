60edab8188219b4a1044152fea765a1db44a7297
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-07-03 15:51:50 +0530

    Add your Posts

--- client/src/components/profile/ProfileEducation.js
+++ client/src/components/profile/ProfileEducation.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 
 const ProfileEducation = ({
-  experience: { school, degree, fieldofstudy, current, to, from, description },
-}) => {
+  education: { school, degree, fieldofstudy, current, to, from, description },
+}) => (
   <div>
     <h3 className='text-dark'>{school}</h3>
     <p>
       <Moment format='YYYY/MM/DD'>{from}</Moment> -{' '}
-      {!to ? 'Now' : <Moment format='YYYY/MM/DD'>{to}</Moment>}
+      {!to ? ' Now' : <Moment format='YYYY/MM/DD'>{to}</Moment>}
     </p>
     <p>
       <strong>Degree: </strong> {degree}
@@ -20,11 +20,11 @@ const ProfileEducation = ({
     <p>
       <strong>Description: </strong> {description}
     </p>
-  </div>;
-};
+  </div>
+);
 
 ProfileEducation.propTypes = {
-  experience: PropTypes.array.isRequired,
+  education: PropTypes.array.isRequired,
 };
 
 export default ProfileEducation;
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,5 @@
 import {
+  ADD_POST,
   DELETE_POST,
   GET_POSTS,
   POST_ERROR,
@@ -20,6 +21,12 @@ export default function (state = initialState, action) {
       return {
         ...state,
         posts: payload,
+        loading: false,
+      };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
         loading: false,
       };
     case DELETE_POST:
